9b0e4007797d1596017e5d093fef47868958b0eb
AbdiIbrahimAdan <abdishu98@gmail.com>
2024-06-22 14:45:45 +0300

    create get, post, patch and delete and testing them and see if it is working in insomnia

--- src/index.js
+++ src/index.js
@@ -3,6 +3,26 @@ import express from 'express';
 
 const app = express();
 
+
+app.get("/product", (req, res) =>{
+    res.send("get all the product");
+})
+
+app.get("/products/:id", (req, res) =>{
+    res.send("get the single product");
+})
+
+
+app.post("/products", (req, res) =>{
+    res.send("create the product");
+})
+app.patch("/products/:id", (req, res) =>{
+    res.send("update the product");
+})
+app.delete("/products/:id", (req, res) =>{
+    res.send("delete all the product");
+})
+
 app.listen(3000, () =>{
     console.log("Server Running on port 3000")
 })
