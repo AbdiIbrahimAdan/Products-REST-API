175ef5bffe37f65cbf16701863e58527161b78fc
AbdiIbrahimAdan <abdishu98@gmail.com>
2024-06-23 15:21:54 +0300

    add validation to input required from the users

--- src/middleware/product.middleware.js
+++ src/middleware/product.middleware.js
@@ -3,4 +3,17 @@ export const productvalidation = (req, res, next) => {
     if (!productThumbnail){
         return res.status(400).json({success:false, message: "product productThumbnail is required"});
     }
-}
+    if (!productTitle ) {
+        return res.status(400).json({ success: false, message: "Product title is required" });
+    }
+    if (typeof productDescription !== 'string' || productDescription.length < 10) {
+        return res.status(400).json({ success: false, message: "Product description must be a string with at least 10 characters" });
+    }
+    if (isNaN(productCost) || Number(productCost) <= 1) {
+        return res.status(400).json({ success: false, message: "Product cost must be a valid number greater than 1" });
+    }
+    if (typeof onOffer !== 'boolean') {
+        return res.status(400).json({ success: false, message: "Product offer status must be a boolean (true/false)" });
+    }
+    next();
+};
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
+import {productvalidation} from '../middleware/product.middleware.js'
 import {getAllProducts, createProduct, getSingleProduct, updateProduct, updateAllProductInfo, deleteProduct} from '../controllers/product.controllers.js';
 const router = Router();
 
-router.get("", getAllProducts ).post("", createProduct).get("/:id", getSingleProduct).patch("/:id", updateProduct).put("/:id", updateAllProductInfo).delete("/:id", deleteProduct)
+router.get("", getAllProducts ).post("", createProduct).get("/:id", getSingleProduct).patch("/:id", productvalidation, updateProduct).put("/:id", productvalidation,updateAllProductInfo).delete("/:id", deleteProduct)
 
 
 export default router;
