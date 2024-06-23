7e21eb0648f156371a356de92a3097078bccbdeb
AbdiIbrahimAdan <abdishu98@gmail.com>
2024-06-23 11:38:29 +0300

    Restructure code into different files to improve readabilty and organization

---
+++ src/controllers/product.controllers.js
@@ -0,0 +1,65 @@
+import pool from "../db.config.js";
+export const getAllProducts = async(req, res) =>{
+    try{
+     const result = await pool.query('SELECT * FROM products');
+     res.json(result.rows);
+    }
+    catch(err){
+     res.status(500).send(err.message)
+    }
+ };
+ export const createProduct = async(req, res) =>{
+    const {productThumbnail, productTitle, productDescription, productCost, onOffer} = req.body;
+    try{
+        const result = await pool.query(
+            'insert into products (productThumbnail, productTitle, productDescription, productCost, onOffer) values($1, $2, $3, $4, $5) RETURNING *',
+            [productThumbnail, productTitle, productDescription, productCost, onOffer]
+        );
+        res.status(201).json(result.rows[0]);
+    }
+    catch(err){
+        res.status(500).send(err.message);
+    }
+};
+export const getSingleProduct =  async(req, res) =>{
+    const {id} = req.params;
+    try{
+        const result = await pool.query('SELECT * FROM products WHERE id = $1', [id]);
+        if (result.rows.length === 0){
+            return res.status(404).send('Product Not Found');
+        }
+        res.json(result.rows[0]);
+    }catch(err){
+        res.status(500).send(err.message);
+    }
+};
+
+export const updateAllProductInfo = async(req, res) =>{
+    const { id } = req.params;
+    const {productThumbnail, productTitle, productDescription, productCost, onOffer} = req.body;
+    try{
+     const result = await pool.query(
+         'UPDATE products SET productThumbnail = $1, productTitle = $2, productDescription = $3, productCost= $4, onOffer = $5 WHERE ID = $6 RETURNING *',
+         [productThumbnail, productTitle, productDescription, productCost, onOffer, id]
+     )
+     if (result.rows.length === 0){
+         return res.status(404).send('Product Not Found');
+     }
+     res.json(result.rows[0]);
+    }catch(err){
+     res.status(500).send(err.message);
+    }
+ };
+
+ export const deleteProduct =  async(req, res) =>{
+    const {id} = req.params;
+    try{
+        const result = await pool.query('DELETE FROM products WHERE id = $1 RETURNING *', [id]);
+        if (result.rows.length === 0){
+            return res.status(404).send('Product Not Found');
+        }
+        res.send('Product deleted Successfully');
+    }catch(err){
+        res.status(500).send(err.message);
+    }
+}; 
